92d13f13f156e0b3d359fe4b0bb712a0a38deb51
Virusrwj223 <mhrishiraj@gmail.com>
2025-03-09 08:40:19 +0800

    Initialise fireabse project

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,19 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+// firebaseClient.ts
+import firebase from "firebase/app";
 
-import * as schema from "./schema";
+import "firebase/firestore"; // or any other services you need
 
-export const db = drizzle({
-  client: sql,
-  schema,
-  casing: "snake_case",
-});
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const firestore = firebase.firestore();
